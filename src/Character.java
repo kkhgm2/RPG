70943c280490b8245a5ad0a597ce3d82da4c6314
kkhgm2 <pma225jkoi7265@yahoo.co.jp>
2019-09-08 15:55:52 +0900

    first

---
+++ src/Character.java
@@ -0,0 +1,32 @@
+
+public class Character {
+	public String name;
+	public int hp;
+	public int mp;
+
+	Character(String name, int hp, int mp){
+		this.name = name;
+		this.hp = hp;
+		this.mp = mp;
+	}
+
+	public void introduce() {
+		System.out.println("私は" + name);
+		System.out.println("HPは" + hp + "です");
+		System.out.println("MPは" + mp + "です");
+	}
+
+	public static void  main(String[] args) {
+		Character c1 = new Character("ドラえもん", 20, 20);
+		Character c2 = new Character("のび太", 10, 10);
+		Character c3 = new Character("ジャイアン", 30, 10);
+		Character c4 = new Character("スネ夫", 15, 15);
+		Character c5 = new Character("しずか", 10, 20);
+		Character c6 = new Character("出木杉", 30, 30);
+
+		c1.introduce();
+		c2.introduce();
+		c3.introduce();
+	}
+
+}
